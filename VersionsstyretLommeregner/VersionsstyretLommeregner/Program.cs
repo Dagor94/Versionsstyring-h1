c8393ff21141c0cd34a5a5e150e9f9c3bb35ac42
joer1 <joer1@elev.techcollege.dk>
2023-08-01 10:06:31 +0200

    Fejlcheck til at verificere brugerinput er et tal

--- VersionsstyretLommeregner/VersionsstyretLommeregner/Program.cs
+++ VersionsstyretLommeregner/VersionsstyretLommeregner/Program.cs
@@ -15,9 +15,21 @@ namespace VersionsstyretLommeregner
             double res = 0;
             string funktion = "";
             Console.Write("Indtast tal: ");
-            tal1 = Convert.ToDouble(Console.ReadLine());
+            string check1 = Console.ReadLine();
+            while (double.TryParse(check1, out double n) == false)
+            {
+                Console.Write("Indtast tal: ");
+                check1 = Console.ReadLine();
+            }
+            tal1 = Convert.ToDouble(check1);
             Console.Write("Indtast tal: ");
-            tal2 = Convert.ToDouble(Console.ReadLine());
+            string check2 = Console.ReadLine();
+            while (double.TryParse(check2, out double n) == false)
+            {
+                Console.Write("Indtast tal: ");
+                check2 = Console.ReadLine();
+            }
+            tal2 = Convert.ToDouble(check2);
             Console.Write("Vælg funktion: ");
             funktion = Console.ReadLine();
             switch (funktion)
